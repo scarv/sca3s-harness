823aa93e7255f2ffe0dab679b4b8250d57682f55
Dan Page <dan@phoo.org>
2021-08-26 12:18:11 +0100

    cosmetic changes (e.g., in documentation); bump submodule

--- src/sca3s/harness/kernel/function/kernel_imp.h
+++ src/sca3s/harness/kernel/function/kernel_imp.h
@@ -53,95 +53,94 @@
 
 // ----------------------------------------------------------------------------
 
-/** @brief      Number of elements in \c x, i.e., the number of  input registers.
+/** @brief      Number of elements in \c x, i.e., the number of  input registers (implying registers \c x[ 0 ] through to \c x[ KERNEL_ELEMOF_X - 1 ] are available), up to a maximum of 8 .
   */
 
-#define KERNEL_ELEMOF_X    2
+#define KERNEL_ELEMOF_X    1
 
-/** @brief      Number of elements in \c r, i.e., the number of output registers.
+/** @brief      Number of elements in \c r, i.e., the number of output registers (implying registers \c r[ 0 ] through to \c r[ KERNEL_ELEMOF_R - 1 ] are available), up to a maximum of 8.
   */
 
 #define KERNEL_ELEMOF_R    1
 
-
 // ----------------------------------------------------------------------------
 
-/** @brief      Type of the 0th  input register `x0` (represented concretly by `x[ 0 ]`).
+/** @brief      Type of the 0th  input register `x0` (represented concretly by `x[ 0 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_X0  KERNEL_DATA_TYPE_I
 
-/** @brief      Type of the 1st  input register `x1` (represented concretly by `x[ 1 ]`).
+/** @brief      Type of the 1st  input register `x1` (represented concretly by `x[ 1 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_X1  KERNEL_DATA_TYPE_I
 
-/** @brief      Type of the 2nd  input register `x2` (represented concretly by `x[ 2 ]`).
+/** @brief      Type of the 2nd  input register `x2` (represented concretly by `x[ 2 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_X2  KERNEL_DATA_TYPE_I
 
-/** @brief      Type of the 3rd  input register `x3` (represented concretly by `x[ 3 ]`).
+/** @brief      Type of the 3rd  input register `x3` (represented concretly by `x[ 3 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_X3  KERNEL_DATA_TYPE_I
 
-/** @brief      Type of the 4th  input register `x4` (represented concretly by `x[ 4 ]`).
+/** @brief      Type of the 4th  input register `x4` (represented concretly by `x[ 4 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_X4  KERNEL_DATA_TYPE_I
 
-/** @brief      Type of the 5th  input register `x5` (represented concretly by `x[ 5 ]`).
+/** @brief      Type of the 5th  input register `x5` (represented concretly by `x[ 5 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_X5  KERNEL_DATA_TYPE_I
 
-/** @brief      Type of the 6th  input register `x6` (represented concretly by `x[ 6 ]`).
+/** @brief      Type of the 6th  input register `x6` (represented concretly by `x[ 6 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_X6  KERNEL_DATA_TYPE_I
 
-/** @brief      Type of the 7th  input register `x7` (represented concretly by `x[ 7 ]`).
+/** @brief      Type of the 7th  input register `x7` (represented concretly by `x[ 7 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_X7  KERNEL_DATA_TYPE_I
 
-/** @brief      Type of the 0th output register `r0` (represented concretly by `r[ 0 ]`).
+/** @brief      Type of the 0th output register `r0` (represented concretly by `r[ 0 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_R0  KERNEL_DATA_TYPE_O
 
-/** @brief      Type of the 1st output register `r1` (represented concretly by `r[ 1 ]`).
+/** @brief      Type of the 1st output register `r1` (represented concretly by `r[ 1 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_R1  KERNEL_DATA_TYPE_O
 
-/** @brief      Type of the 2nd output register `r2` (represented concretly by `r[ 2 ]`).
+/** @brief      Type of the 2nd output register `r2` (represented concretly by `r[ 2 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_R2  KERNEL_DATA_TYPE_O
 
-/** @brief      Type of the 3rd output register `r3` (represented concretly by `r[ 3 ]`).
+/** @brief      Type of the 3rd output register `r3` (represented concretly by `r[ 3 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_R3  KERNEL_DATA_TYPE_O
 
-/** @brief      Type of the 4th output register `r4` (represented concretly by `r[ 4 ]`).
+/** @brief      Type of the 4th output register `r4` (represented concretly by `r[ 4 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_R4  KERNEL_DATA_TYPE_O
 
-/** @brief      Type of the 5th output register `r5` (represented concretly by `r[ 5 ]`).
+/** @brief      Type of the 5th output register `r5` (represented concretly by `r[ 5 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_R5  KERNEL_DATA_TYPE_O
 
-/** @brief      Type of the 6th output register `r6` (represented concretly by `r[ 6 ]`).
+/** @brief      Type of the 6th output register `r6` (represented concretly by `r[ 6 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_R6  KERNEL_DATA_TYPE_O
 
-/** @brief      Type of the 7th output register `r7` (represented concretly by `r[ 7 ]`).
+/** @brief      Type of the 7th output register `r7` (represented concretly by `r[ 7 ]`) when available.
   */
 
 #define KERNEL_TYPEOF_R7  KERNEL_DATA_TYPE_O
